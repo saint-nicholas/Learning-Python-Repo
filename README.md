58f2d53574d1eff38d78f3a055a718cfc0326468
saint-nicholas <nick.hunsaker7@gmail.com>
2022-10-26 09:46:42 -0700

    First python commit
    First commit to my learning python repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# Learning Python Repo
+I created this repo to document my journey of learning Python. The first file I added (python_practice.py) is some of the first python code I have written. 
---
+++ python_practice.py
@@ -0,0 +1,13 @@
+print("Hello there !!!!!")
+
+name = input("What is your name?\n")
+
+print("Hello there " + name + ". Welcome to the coffee shop")
+
+menu = "Coffee, Lattes, Croissants, Bagels and Eggs"
+
+print("Today we have " + menu)
+
+order = input("What would you like to order?\n")
+
+print("Ok " + name + " you ordered " + order + "\nWe'll have that right out for you!")
