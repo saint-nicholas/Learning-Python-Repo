7c58ecc685082f6a7abc842e5498ac6d7989fe20
saint-nicholas <nick.hunsaker7@gmail.com>
2022-12-05 16:12:29 -0800

    Video 3
    Updated the order taking script to ask the customer for the number of items that they wish to order and tell them the price based on their answer.

--- python_practice.py
+++ python_practice.py
@@ -11,4 +11,13 @@ print("Today we have " + menu) #Relaying the available menu items to the custome
 
 order = input("What would you like to order?\n") #Getting the customer's order
 
-print("Ok " + name + " you ordered " + order + "\nWe'll have that right out for you!") #Confirming that the customer's order has been submitted
+price = 7 #assigning the same price to all menu items for the sake of simplicity
+
+number_of_items = input("How many " + order + "s would you like?\n") #asking the customer how many items they would like
+
+total = int(number_of_items) * price #calculating the total price based on the number of items ordered
+
+print("Ok " + name + " you ordered " + number_of_items + " " + order + "s. Your total is going to come to $" + str(total) + ".\nWe'll have that right out for you!") #Confirming that the customer's order has been submitted and giving them their total
+
+
+
